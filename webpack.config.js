f3e9d439ff534eb6cbf26f44cb9feeb1e4cebfff
deanchen <antlet2004@gmail.com>
2021-10-28 17:07:52 +0800

    feat: can run dev server with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 5000,
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
