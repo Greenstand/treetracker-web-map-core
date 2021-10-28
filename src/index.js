2a481ce21e36ef454d413f9db08490d56506686d
deanchen <antlet2004@gmail.com>
2021-10-28 17:37:59 +0800

    feat: can load the part of the map, but can not work well

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import _ from "lodash";
+import 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-utfgrid/L.UTFGrid';
+import 'leaflet.gridlayer.googlemutant';
+import Map from "./Map.js";
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack serve!'], ' ');
 
   return element;
 }
 
-document.body.appendChild(component());
+const element = component();
+
+document.body.appendChild(element);
+
+const mapContainer = document.createElement('div');
+mapContainer.innerHTML = "<div id='map' style='height: 600px;width: 800px;' ></div>";
+document.body.appendChild(mapContainer);
+
+window.onload = () => {
+  const map = new Map({
+    onLoad: () => console.log("onload"),
+    onClickTree: () => console.log("onClickTree"),
+    onFindNearestAt: () => console.log("onFindNearstAt"),
+    onError: () => console.log("onError"),
+    filters: {},
+  });
+  map.mount(document.getElementById("map"));
+};
