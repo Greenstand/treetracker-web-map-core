2e80185f0333f0b1e8942e3bfa7caccfea212f83
deanchen <antlet2004@gmail.com>
2021-11-11 12:08:55 +0800

    chore: some basic code

--- cypress/integration/map.spec.js
+++ cypress/integration/map.spec.js
@@ -15,5 +15,14 @@ describe("Map", () => {
     cy.contains("Welcome");
   });
 
+  it("to check if we can update the bounds correctly", () => {
+    cy.visit("http://localhost:5000");
+    cy.contains("Welcome");
+    cy.wait(1000);
+    cy.get('input[name=userid]').type(940);
+    cy.get('input[name=submit]')
+      .click();
+  });
+
 
 });
