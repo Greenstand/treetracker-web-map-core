6ffc3e33355b7282a898afd60da0ca529ea9905f
deanchen <antlet2004@gmail.com>
2021-11-11 14:53:13 +0800

    feat: support onMoveEnd event

--- cypress/integration/map.spec.js
+++ cypress/integration/map.spec.js
@@ -10,18 +10,21 @@ describe("Map", () => {
       .click();
   });
   
-  it.skip("map userid case", () => {
+  it("map userid case", () => {
     cy.visit("http://localhost:5000?userid=940");
     cy.contains("Welcome");
   });
 
-  it("to check if we can update the bounds correctly", () => {
+  it.only("to check if we can update the bounds correctly", () => {
     cy.visit("http://localhost:5000");
     cy.contains("Welcome");
     cy.wait(1000);
-    cy.get('input[name=userid]').type(940);
+    cy.get('input[name=lat]').type(0);
+    cy.get('input[name=lon]').type(0);
+    cy.get('input[name=zoom]').type(5);
     cy.get('input[name=flytoSubmit]')
       .click();
+    cy.url().should('match',/bounds=/);
   });
 
 
