ebe22dcbac86a65c0f3aec177bfd1f63c0539593
deanchen <antlet2004@gmail.com>
2021-11-11 10:34:35 +0800

    feat: be able to switch initial view

--- cypress/integration/map.spec.js
+++ cypress/integration/map.spec.js
@@ -4,6 +4,16 @@ describe("Map", () => {
   it("map", () => {
     cy.visit("http://localhost:5000");
     cy.contains("Welcome");
+    cy.wait(1000);
+    cy.get('input[name=userid]').type(940);
+    cy.get('input[name=submit]')
+      .click();
   });
+  
+  it.skip("map userid case", () => {
+    cy.visit("http://localhost:5000?userid=940");
+    cy.contains("Welcome");
+  });
+
 
 });
