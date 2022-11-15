db734d89a3aa801939a7b149303cdd812357f42f
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-11-14 23:36:53 -0800

    chore: add types for TileLoadingMonitor

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,17 @@
+import TileLoadingMonitor from './TileLoadingMonitor'
+
 export type DirectionType = 'north' | 'south' | 'east' | 'west'
 
 export type CoordinatesType = {
   lat: number
   lng: number
+}
+
+export type TileLoadingMonitorOptions = {
+  showLoadingThreshold: number
+  slowThreshold: number
+  onShowLoading: () => void
+  onSlowAlert: () => void
+  onLoad: () => void
+  onDestroy?: () => void
 }
