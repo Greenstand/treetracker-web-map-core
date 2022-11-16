dd7a9c964ddb96c0a77dd6ef12e0a51ac235746a
Jafer Ahmed <op.jafer.ahmed@gmail.com>
2022-11-15 17:26:08 -0800

    chore: add types for tileLoadingMonitor test file

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,10 @@
+import TileLoadingMonitor from './TileLoadingMonitor'
+
 export type DirectionType = 'north' | 'south' | 'east' | 'west'
 
 export type CoordinatesType = {
   lat: number
   lng: number
 }
+
+export type TileLoadingMonitorType = InstanceType<typeof TileLoadingMonitor>
